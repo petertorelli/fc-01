8d58ff503ad3517235af0e60e71068c000c3ba15
petertorelli <peter.j.torelli@gmail.com>
2021-05-09 16:04:49 -0700

    Added GPL3.0

--- src/renderer/components/Rotor.vue
+++ src/renderer/components/Rotor.vue
@@ -1,3 +1,20 @@
+<!--
+An experimental playground for testing quadrotor flight control.
+Copyright (C) 2021 Peter J. Torelli <peter.j.torelli@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <template lang='pug'>
 mixin arrows
 	div &#x25b2;
@@ -25,6 +42,7 @@ div
 </template>
 
 <script>
+
 export default {
 	name: 'Rotor',
 	props: [ 'text',  'val' ]
--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,5 +1,3 @@
-/* TODO: I don't understand these shim files I keep seeing... */
-
 declare module "*.vue" {
     import Vue from "vue";
     export default Vue;
